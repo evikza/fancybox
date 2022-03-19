008f83e437363e701da1645a4c201081b22ba8ba
evikza <48568977+evikza@users.noreply.github.com>
2022-03-19 21:18:55 +0500

    Added embeded video rutube.ru

--- src/js/media.js
+++ src/js/media.js
@@ -5,26 +5,27 @@
 //
 // ==========================================================================
 (function ($) {
-  "use strict";
+  'use strict';
 
   // Object containing properties for each media type
   var defaults = {
     youtube: {
-      matcher: /(youtube\.com|youtu\.be|youtube\-nocookie\.com)\/(watch\?(.*&)?v=|v\/|u\/|embed\/?)?(videoseries\?list=(.*)|[\w-]{11}|\?listType=(.*)&list=(.*))(.*)/i,
+      matcher:
+        /(youtube\.com|youtu\.be|youtube\-nocookie\.com)\/(watch\?(.*&)?v=|v\/|u\/|embed\/?)?(videoseries\?list=(.*)|[\w-]{11}|\?listType=(.*)&list=(.*))(.*)/i,
       params: {
         autoplay: 1,
         autohide: 1,
         fs: 1,
         rel: 0,
         hd: 1,
-        wmode: "transparent",
+        wmode: 'transparent',
         enablejsapi: 1,
-        html5: 1
+        html5: 1,
       },
       paramPlace: 8,
-      type: "iframe",
-      url: "https://www.youtube-nocookie.com/embed/$4",
-      thumb: "https://img.youtube.com/vi/$4/hqdefault.jpg"
+      type: 'iframe',
+      url: 'https://www.youtube-nocookie.com/embed/$4',
+      thumb: 'https://img.youtube.com/vi/$4/hqdefault.jpg',
     },
 
     vimeo: {
@@ -35,17 +36,27 @@
         show_title: 1,
         show_byline: 1,
         show_portrait: 0,
-        fullscreen: 1
+        fullscreen: 1,
       },
       paramPlace: 3,
-      type: "iframe",
-      url: "//player.vimeo.com/video/$2"
+      type: 'iframe',
+      url: '//player.vimeo.com/video/$2',
+    },
+
+    rutube: {
+      matcher: /^.+rutube.ru\/video\/(.*\/)?([\w]+)(.*)\/?/,
+      params: {
+        frameBorder: 0,
+      },
+      paramPlace: 1,
+      type: 'iframe',
+      url: '//rutube.ru/play/embed/$2',
     },
 
     instagram: {
       matcher: /(instagr\.am|instagram\.com)\/p\/([a-zA-Z0-9_\-]+)\/?/i,
-      type: "image",
-      url: "//$1/p/$2/media/?size=l"
+      type: 'image',
+      url: '//$1/p/$2/media/?size=l',
     },
 
     // Examples:
@@ -54,18 +65,25 @@
     // https://www.google.com/maps/@52.2111123,2.9237542,6.61z?hl=en
     // https://www.google.com/maps/place/Googleplex/@37.4220041,-122.0833494,17z/data=!4m5!3m4!1s0x0:0x6c296c66619367e0!8m2!3d37.4219998!4d-122.0840572
     gmap_place: {
-      matcher: /(maps\.)?google\.([a-z]{2,3}(\.[a-z]{2})?)\/(((maps\/(place\/(.*)\/)?\@(.*),(\d+.?\d+?)z))|(\?ll=))(.*)?/i,
-      type: "iframe",
+      matcher:
+        /(maps\.)?google\.([a-z]{2,3}(\.[a-z]{2})?)\/(((maps\/(place\/(.*)\/)?\@(.*),(\d+.?\d+?)z))|(\?ll=))(.*)?/i,
+      type: 'iframe',
       url: function (rez) {
         return (
-          "//maps.google." +
+          '//maps.google.' +
           rez[2] +
-          "/?ll=" +
-          (rez[9] ? rez[9] + "&z=" + Math.floor(rez[10]) + (rez[12] ? rez[12].replace(/^\//, "&") : "") : rez[12] + "").replace(/\?/, "&") +
-          "&output=" +
-          (rez[12] && rez[12].indexOf("layer=c") > 0 ? "svembed" : "embed")
+          '/?ll=' +
+          (rez[9]
+            ? rez[9] +
+              '&z=' +
+              Math.floor(rez[10]) +
+              (rez[12] ? rez[12].replace(/^\//, '&') : '')
+            : rez[12] + ''
+          ).replace(/\?/, '&') +
+          '&output=' +
+          (rez[12] && rez[12].indexOf('layer=c') > 0 ? 'svembed' : 'embed')
         );
-      }
+      },
     },
 
     // Examples:
@@ -73,12 +91,19 @@
     // https://www.google.com/maps/search/?api=1&query=centurylink+field
     // https://www.google.com/maps/search/?api=1&query=47.5951518,-122.3316393
     gmap_search: {
-      matcher: /(maps\.)?google\.([a-z]{2,3}(\.[a-z]{2})?)\/(maps\/search\/)(.*)/i,
-      type: "iframe",
+      matcher:
+        /(maps\.)?google\.([a-z]{2,3}(\.[a-z]{2})?)\/(maps\/search\/)(.*)/i,
+      type: 'iframe',
       url: function (rez) {
-        return "//maps.google." + rez[2] + "/maps?q=" + rez[5].replace("query=", "q=").replace("api=1", "") + "&output=embed";
-      }
-    }
+        return (
+          '//maps.google.' +
+          rez[2] +
+          '/maps?q=' +
+          rez[5].replace('query=', 'q=').replace('api=1', '') +
+          '&output=embed'
+        );
+      },
+    },
   };
 
   // Formats matching url to final form
@@ -87,25 +112,25 @@
       return;
     }
 
-    params = params || "";
+    params = params || '';
 
-    if ($.type(params) === "object") {
+    if ($.type(params) === 'object') {
       params = $.param(params, true);
     }
 
     $.each(rez, function (key, value) {
-      url = url.replace("$" + key, value || "");
+      url = url.replace('$' + key, value || '');
     });
 
     if (params.length) {
-      url += (url.indexOf("?") > 0 ? "&" : "?") + params;
+      url += (url.indexOf('?') > 0 ? '&' : '?') + params;
     }
 
     return url;
   };
 
-  $(document).on("objectNeedsType.fb", function (e, instance, item) {
-    var url = item.src || "",
+  $(document).on('objectNeedsType.fb', function (e, instance, item) {
+    var url = item.src || '',
       type = false,
       media,
       thumb,
@@ -132,35 +157,47 @@
       if (providerOpts.paramPlace && rez[providerOpts.paramPlace]) {
         urlParams = rez[providerOpts.paramPlace];
 
-        if (urlParams[0] == "?") {
+        if (urlParams[0] == '?') {
           urlParams = urlParams.substring(1);
         }
 
-        urlParams = urlParams.split("&");
+        urlParams = urlParams.split('&');
 
         for (var m = 0; m < urlParams.length; ++m) {
-          var p = urlParams[m].split("=", 2);
+          var p = urlParams[m].split('=', 2);
 
           if (p.length == 2) {
-            paramObj[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+            paramObj[p[0]] = decodeURIComponent(p[1].replace(/\+/g, ' '));
           }
         }
       }
 
-      params = $.extend(true, {}, providerOpts.params, item.opts[providerName], paramObj);
+      params = $.extend(
+        true,
+        {},
+        providerOpts.params,
+        item.opts[providerName],
+        paramObj
+      );
 
       url =
-        $.type(providerOpts.url) === "function" ? providerOpts.url.call(this, rez, params, item) : format(providerOpts.url, rez, params);
+        $.type(providerOpts.url) === 'function'
+          ? providerOpts.url.call(this, rez, params, item)
+          : format(providerOpts.url, rez, params);
 
       thumb =
-        $.type(providerOpts.thumb) === "function" ? providerOpts.thumb.call(this, rez, params, item) : format(providerOpts.thumb, rez);
+        $.type(providerOpts.thumb) === 'function'
+          ? providerOpts.thumb.call(this, rez, params, item)
+          : format(providerOpts.thumb, rez);
 
-      if (providerName === "youtube") {
+      if (providerName === 'youtube') {
         url = url.replace(/&t=((\d+)m)?(\d+)s/, function (match, p1, m, s) {
-          return "&start=" + ((m ? parseInt(m, 10) * 60 : 0) + parseInt(s, 10));
+          return '&start=' + ((m ? parseInt(m, 10) * 60 : 0) + parseInt(s, 10));
         });
-      } else if (providerName === "vimeo") {
-        url = url.replace("&%23", "#");
+      } else if (providerName === 'vimeo') {
+        url = url.replace('&%23', '#');
+      } else if (providerName === 'rutube') {
+        url = url.replace('&%23', '#');
       }
 
       return false;
@@ -173,14 +210,14 @@
         item.opts.thumb = thumb;
       }
 
-      if (type === "iframe") {
+      if (type === 'iframe') {
         item.opts = $.extend(true, item.opts, {
           iframe: {
             preload: false,
             attr: {
-              scrolling: "no"
-            }
-          }
+              scrolling: 'no',
+            },
+          },
         });
       }
 
@@ -189,7 +226,12 @@
         src: url,
         origSrc: item.src,
         contentSource: provider,
-        contentType: type === "image" ? "image" : provider == "gmap_place" || provider == "gmap_search" ? "map" : "video"
+        contentType:
+          type === 'image'
+            ? 'image'
+            : provider == 'gmap_place' || provider == 'gmap_search'
+            ? 'map'
+            : 'video',
       });
     } else if (url) {
       item.type = item.opts.defaultType;
@@ -199,17 +241,24 @@
   // Load YouTube/Video API on request to detect when video finished playing
   var VideoAPILoader = {
     youtube: {
-      src: "https://www.youtube.com/iframe_api",
-      class: "YT",
+      src: 'https://www.youtube.com/iframe_api',
+      class: 'YT',
       loading: false,
-      loaded: false
+      loaded: false,
     },
 
     vimeo: {
-      src: "https://player.vimeo.com/api/player.js",
-      class: "Vimeo",
+      src: 'https://player.vimeo.com/api/player.js',
+      class: 'Vimeo',
       loading: false,
-      loaded: false
+      loaded: false,
+    },
+
+    rutube: {
+      src: 'https://static.rutube.ru/static/player_sdk/hls.min.js',
+      class: 'Rutube',
+      loading: false,
+      loaded: false,
     },
 
     load: function (vendor) {
@@ -229,11 +278,11 @@
 
       this[vendor].loading = true;
 
-      script = document.createElement("script");
-      script.type = "text/javascript";
+      script = document.createElement('script');
+      script.type = 'text/javascript';
       script.src = this[vendor].src;
 
-      if (vendor === "youtube") {
+      if (vendor === 'youtube') {
         window.onYouTubeIframeAPIReady = function () {
           _this[vendor].loaded = true;
           _this.done(vendor);
@@ -250,41 +299,46 @@
     done: function (vendor) {
       var instance, $el, player;
 
-      if (vendor === "youtube") {
+      if (vendor === 'youtube') {
         delete window.onYouTubeIframeAPIReady;
       }
 
       instance = $.fancybox.getInstance();
 
       if (instance) {
-        $el = instance.current.$content.find("iframe");
+        $el = instance.current.$content.find('iframe');
 
-        if (vendor === "youtube" && YT !== undefined && YT) {
-          player = new YT.Player($el.attr("id"), {
+        if (vendor === 'youtube' && YT !== undefined && YT) {
+          player = new YT.Player($el.attr('id'), {
             events: {
               onStateChange: function (e) {
                 if (e.data == 0) {
                   instance.next();
                 }
-              }
-            }
+              },
+            },
           });
-        } else if (vendor === "vimeo" && Vimeo !== undefined && Vimeo) {
+        } else if (vendor === 'vimeo' && Vimeo !== undefined && Vimeo) {
           player = new Vimeo.Player($el);
 
-          player.on("ended", function () {
+          player.on('ended', function () {
             instance.next();
           });
         }
       }
-    }
+    },
   };
 
   $(document).on({
-    "afterShow.fb": function (e, instance, current) {
-      if (instance.group.length > 1 && (current.contentSource === "youtube" || current.contentSource === "vimeo")) {
+    'afterShow.fb': function (e, instance, current) {
+      if (
+        instance.group.length > 1 &&
+        (current.contentSource === 'youtube' ||
+          current.contentSource === 'vimeo' ||
+          current.contentSource === 'rutube')
+      ) {
         VideoAPILoader.load(current.contentSource);
       }
-    }
+    },
   });
 })(jQuery);
